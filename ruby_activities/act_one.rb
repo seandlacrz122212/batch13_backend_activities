232589e40ea4b8c07f256bc90f339bc633c1662b
Sean Dela Cruz <andreadlacrz1@gmail.comm>
2022-01-13 21:44:30 +0800

    Activities

---
+++ ruby_activities/act_one.rb
@@ -0,0 +1,45 @@
+# Activity 1
+
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each do |item|
+#     puts item
+#    end
+
+#-------------------------------------------------------
+
+# Activity 2
+
+# h = {a:1, b:2, c:3, d:4}
+# h[:b]
+
+# h[:e] = 5
+
+#-------------------------------------------------------
+
+# Activity 3
+
+# contact_data = [["john@email.com", "123 Main st.", "555-123-4567"],
+#             ["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+# contacts = {"John Cruz" => {}, "Avion School" => {}}
+
+# contacts["John Cruz"][:email] = contact_data [0][0]
+# contacts["John Cruz"][:address] = contact_data [0][1]
+# contacts["John Cruz"][:phone] = contact_data [0][2]
+# contacts["Avion School"][:email] = contact_data [1][0]
+# contacts["Avion School"][:address] = contact_data [1][1]
+# contacts["Avion School"][:phone] = contact_data [1][2]
+
+
+#-------------------------------------------------------
+
+# Activity 4
+
+# def ask_age 
+#     puts "How old are you?"
+#     input = gets.chomp.to_i
+# puts "You'll be #{input+10} in 10 Years"
+# puts "You'll be #{input+20} in 20 Years"
+# puts "You'll be #{input+30} in 30 Years"
+# puts "You'll be #{input+40} in 40 Years"
+
+# end
