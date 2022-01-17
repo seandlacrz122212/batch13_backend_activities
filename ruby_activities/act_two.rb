406c001bfb31b61f633e498d02f901f87f3a9d15
Sean Dela Cruz <andreadlacrz1@gmail.comm>
2022-01-17 14:19:09 +0800

    Activities

---
+++ ruby_activities/act_two.rb
@@ -0,0 +1,59 @@
+# Activity 1
+
+# arr = [1,3,5,7,9,11]
+# number = 9
+
+#     arr.each do |x|
+#         if x == number
+#             puts "#{number} is part of the array"
+#         end
+#     end
+
+#-------------------------------------------------------
+
+# Activity 2
+
+# def check_num_range(num)
+#     unless num.class == Integer
+#         return "#{num} is not an integer!"
+#     end
+
+#     case num 
+#     when 0..50
+#         return 'Number is between 0 and 50'
+#     when 51..100
+#         return 'Number is between 51 and 100'
+#     else
+#         return 'Number is above 100'
+#     end
+# end
+
+#-------------------------------------------------------
+
+# Activity 3
+
+# def loop_stop
+#     loop do
+#         puts "ENter STOP to end loop: "
+#         input = gets
+#         if input == "STOP\n"
+#         break
+#         end
+#     end
+# end
+
+
+#-------------------------------------------------------
+
+# Activity 4
+
+# def myfunction
+#     array = [6,3,1,8,4,2,10,65,102]
+#     divisible = []
+#     array.each do |x|
+#         if x % 2 == 0
+#             divisible.push(X)
+#         end
+#     end
+#     puts divisible
+# end
