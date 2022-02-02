e76cd2826d0dbf374d9aabe9db0aff4bb458b2c2
Sean Dela Cruz <andreadlacrz1@gmail.comm>
2022-02-02 15:40:59 +0800

    Activity

--- ruby_activities/database/act_six.sql
+++ ruby_activities/database/act_six.sql
@@ -41,3 +41,45 @@ SELECT AVG(age) FROM students;
 
 
 SELECT * FROM students ORDER BY age DESC;
+
+----------------------------------
+
+-- PT 2
+
+CREATE table notes (
+	id integer PRIMARY KEY, 
+	body character varying(255),
+	student_id integer); 
+	
+INSERT INTO notes (id, body, student_id)
+VALUES
+    (1, 'Passed', 1),
+    (2, 'Passed', 2),
+    (3, 'Retake Exam', 3),
+    (4, 'Error', NULL),
+    (5, 'Passed', 5),
+    (6, 'Passed', 6),
+    (7, 'Retake Exam', 7),
+    (8, 'Passed', 8),
+    (9, 'Error', NULL),
+    (10, 'Passed', 10);
+ 
+SELECT students.first_name, students.last_name, notes.body
+FROM students
+INNER JOIN notes 
+ON students.id = notes.student_id;
+
+SELECT students.first_name, students.last_name, notes.body
+FROM students
+LEFT JOIN notes
+ON students.id = notes.student_id;
+
+SELECT students.first_name, students.last_name, notes.body
+FROM students
+right JOIN notes
+ON students.id = notes.student_id;
+
+SELECT students.first_name, students.last_name, notes.body
+FROM students
+FULL OUTER JOIN notes 
+ON students.id = notes.student_id;
